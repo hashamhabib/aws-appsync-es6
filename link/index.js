465cb28adca4a96853bc359a9f75e91a2a26b971
Hasham Habib <itshashamhabib@gmail.com>
2019-07-18 17:52:43 +1200

    Update index.js

--- link/index.js
+++ link/index.js
@@ -1,28 +1,21 @@
 
-// var auth_link_1 = require("./auth-link");
-import * as auth_link_1 from './auth-link';
-export const AuthLink = auth_link_1.AuthLink;
-export const AUTH_TYPE = auth_link_1.AUTH_TYPE;
+import * as auth_link_1 from './auth-link'
+import * as offline_link_1 from './offline-link'
+import * as subscription_handshake_link_1 from './subscription-handshake-link'
 
-import * as offline_link_1 from './offline-link';
-// var offline_link_1 = require("./offline-link");
+import * as non_terminating_http_link_1 from './non-terminating-http-link'
+import * as non_terminating_link_1 from './non-terminating-link'
 
-export const OfflineLink = offline_link_1.OfflineLink;
-export const replaceUsingMap = offline_link_1.replaceUsingMap;
-// var subscription_handshake_link_1 = require("./subscription-handshake-link");
+import * as complex_object_link_1 from './complex-object-link'
+import * as signer_1 from './signer'
 
-import * as subscription_handshake_link_1 from './subscription-handshake-link';
+export const AuthLink = auth_link_1.AuthLink
+export const AUTH_TYPE = auth_link_1.AUTH_TYPE
+export const OfflineLink = offline_link_1.OfflineLink
+export const replaceUsingMap = offline_link_1.replaceUsingMap
 
-export const SubscriptionHandshakeLink = subscription_handshake_link_1.SubscriptionHandshakeLink;
-// var non_terminating_http_link_1 = require("./non-terminating-http-link");
-import * as non_terminating_http_link_1 from './non-terminating-http-link';
-export const NonTerminatingHttpLink = non_terminating_http_link_1.NonTerminatingHttpLink;
-// var non_terminating_link_1 = require("./non-terminating-link");
-import * as non_terminating_link_1 from './non-terminating-link';
-export const NonTerminatingLink = non_terminating_link_1.NonTerminatingLink;
-// var complex_object_link_1 = require("./complex-object-link");
-import * as complex_object_link_1 from './complex-object-link';
-export const ComplexObjectLink = complex_object_link_1.ComplexObjectLink;
-// var signer_1 = require("./signer");
-import * as signer_1 from './signer';
-export const Signer = signer_1.Signer;
+export const SubscriptionHandshakeLink = subscription_handshake_link_1.SubscriptionHandshakeLink
+export const NonTerminatingHttpLink = non_terminating_http_link_1.NonTerminatingHttpLink
+export const NonTerminatingLink = non_terminating_link_1.NonTerminatingLink
+export const ComplexObjectLink = complex_object_link_1.ComplexObjectLink
+export const Signer = signer_1.Signer
